8ebdf34182374566b5eba16ddd9f937be2d13998
RaiVitorianoNogueira <rainogueira2019@gmail.com>
2025-12-14 18:30:11 +0000

    terminei zoologico

--- tasks/poo/zoo/py/draft.py
+++ tasks/poo/zoo/py/draft.py
@@ -2,20 +2,31 @@ from typing import Any
 from abc import ABC, abstractmethod
 
 class Brinquedo(ABC):
+    def __init__(self, nome: str = "") -> None:
+        self.nome = nome
+
     @abstractmethod
     def apertar(self):
         pass
 
     @abstractmethod
     def jogar_no_chao(self):    
-        pass
+        pass 
 
-def brincar(briquedo: Any):
+    
+
+def brincar(briquedo: Brinquedo):
+    brinquedo.apertar()
+    brinquedo.jogar_no_chao()
+    print(brinquedo)
 
 
 
-class Ursinho(Brinquedo) -> None:
-    def apertar(self):
+class Ursinho(Brinquedo):
+
+    def __init__(self) -> None:
+        self.nome = "Ursinho de pelúcia"
+    def apertar(self) -> None:
         print("O ursinho é macio!")
 
     def jogar_no_chao(self) -> None:
@@ -26,5 +37,14 @@ class Ursinho(Brinquedo) -> None:
     
 
 
-class Chinela()
+class Mordedor(Brinquedo):
+    def apertar(self) -> None:
+        print("O mordedor faiz barulho")
+
+class Chinela():
+    def apertar(self) -> None:
+
+
+
+
     
